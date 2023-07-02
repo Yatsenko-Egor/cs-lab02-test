9a8fe15c55b78c47e955e6ea626121f852ff30c8
YatsenkoEV <gretchin777@gmail.com>
2023-07-02 12:35:30 +0300

    code: добавлен поиск максимального числа

--- project.cpp
+++ project.cpp
@@ -11,4 +11,8 @@ int main()
 		 << "A - B = " << a - b << '\n'
 		 << "A * B = " << a * b << '\n'
 		 << "A / B = " << a / b << '\n';
+	if (a > b)
+		cout << "max: " << a << '\n';
+	else
+		cout << "max: " << b << '\n';
 }
